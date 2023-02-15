51392def627c3d7953642aac22ac8088d39f34d5
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-02-15 16:22:09 +0300

    init

---
+++ morbidity.js
@@ -0,0 +1,74 @@
+(function () {
+    const parent = document.querySelector(".morbidity");
+    if (!parent) return;
+
+    new Swiper(".morbidity__slider", {
+        autoplay: {
+            delay: 5000,
+        },
+        navigation: {
+            nextEl: ".sl-arrow__btn--next",
+            prevEl: ".sl-arrow__btn--prev",
+        },
+    });
+
+    var dataFirst = {
+        label: "Заболеваемость 2022/23",
+        data: [0, 59, 75, 20, 20, 55, 40],
+        fill: false,
+    };
+
+    var dataSecond = {
+        label: "Базовая линия 2022/23",
+        data: [-10, 15, 60, 60, 110, 100, null],
+        fill: false,
+    };
+
+    var sickData = {
+        labels: [0, 10, 20, 30, 40, 50, 60],
+        datasets: [dataFirst, dataSecond],
+    };
+
+    var chartOptions = {
+        plugins: {
+            legend: {
+                display: true,
+                position: "bottom",
+                labels: {
+                    boxWidth: 100,
+                },
+            },
+        },
+        scales: {
+            x: {
+                display: true,
+                title: {
+                    display: true,
+                    text: "Ось X",
+                },
+            },
+            y: {
+                display: true,
+                title: {
+                    display: true,
+                    text: "Ось Y",
+                },
+            },
+        },
+    };
+
+    var sickCanvas = document.querySelector("#sickChart");
+    var sickCanvas2 = document.querySelector("#sickChart2");
+
+    new Chart(sickCanvas, {
+        type: "line",
+        data: sickData,
+        options: chartOptions,
+    });
+
+    new Chart(sickCanvas2, {
+        type: "line",
+        data: sickData,
+        options: chartOptions,
+    });
+})();
